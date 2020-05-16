b4a1dd3d400a3aab9ec24401ab7f7b663f03ffac
Varun Mehta <varun.mehta02@nagarro.com>
2020-05-16 18:46:42 +0530

    cart added

--- src/Components/cart/cartItem.js
+++ src/Components/cart/cartItem.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 function CartItem() {
-  return <div>cart item</div>;
+  return (
+    <div className="row">
+      <div className="col ml-4 mt-4 mb-4 mr-4">
+        <img
+          src={require('../../assets/images/apple-iphone-xr.jpeg')}
+          height="128"
+          width="64"
+        />
+      </div>
+      <div className="col"></div>
+    </div>
+  );
 }
 
 export default CartItem;
--- src/Components/cart/index.js
+++ src/Components/cart/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import cart from '../../assets/icons/cart.png';
+import CartItem from './cartItem';
 
 function Cart() {
-  return <div>cart</div>;
+  return (
+    <div
+      className="container text-monospace mt-5 border border-dark bg-light"
+      style={{ width: '50rem', padding: '0rem' }}
+    >
+      <nav className="navbar navbar-dark bg-dark">
+        <span className="navbar-brand mb-0 h1">
+          <img className="mr-3" src={cart} heigth="28" width="28" />
+          Shipping Cart
+        </span>
+      </nav>
+      <CartItem />
+      <hr />
+      <CartItem />
+    </div>
+  );
 }
 
 export default Cart;
